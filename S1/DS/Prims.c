a20b4c59d8f0cc1f643ae536ca317a460e00ebb5
ARSHA PURUSHAN <147630900+ArshaPurushan@users.noreply.github.com>
2023-11-21 11:43:30 +0530

    Add files via upload

--- S1/DS/Prims.c
+++ S1/DS/Prims.c
@@ -1,68 +1,99 @@
 #include<stdio.h>
+#include<stdlib.h>
+ 
+#define infinity 9999
+#define MAX 20
+ 
+int G[MAX][MAX],spanning[MAX][MAX],n;
+ 
+int prims();
+ 
 int main()
 {
-        int t[4][2];
-        int near[5];
-        int n=5,j;
-        int cost[5][5]={{999,5,8,999,999},{5,999,999,3,4},{8,999,999,6,999},{999,3,6,999,2},{999,4,999,2,999}};
-        int mincost=cost[4][3];
-        int min=999,min_j;
-        //the vertices are 5 and for but in matrix start from 0
-        int k=4,l=3,i;
-        //since the vertixes are 5 and 4 respectively
-        t[0][0]=k+1;
-        t[0][1]=l+1;
-        for(i=0;i<n;i++)
-        {
-            if(cost[i,l]<cost[i,k])
-                near[i]=l;
-            else
-                near[i]=k;
-            
-        }
-        near[k]=near[l]=0;
-        printf("near of after first iteration \n");
-        for(i=0;i<n;i++)
-            printf("%d \t",near[i]);
-        //here near[]={3,3,3,0,0}
-        for(i=1;i<n-1;i++)
-            {
-                //to find j such that near[j]!=0 and cost[j,near[j]] is minimum
-                for(j=0;j<n;j++)
-                {
-                   if(near[j]!=0 && min>cost[j][near[j]])
-                   {
-                     min=near[j];
-                     min_j=j;
-                     //since both are same we remove a value 
-                    
-                   }
-                }
-                min=999;
-                j=min_j;
-                printf("minimumcost in %d th iteration %d \n",i,cost[j][near[j]]);
-                t[i][0]=j+1;
-                t[i][1]=near[j]+1;
-                mincost=mincost+cost[j][near[j]];
-                near[j]=0;
-                for(k=2;k<n;k++)
-                  {
-                  if(near[k]!=0 && (cost[k][near[k]]>cost[k][j]))
-                      near[k]=j;
-                  }
-            }
-          printf("minimumcost=%d \n",mincost);
-          for(j=0;j<n-1;j++)
-          	{
-          	printf("%d \t %d \n",t[j][0],t[j][1]);
-          	}
-          return 0;
-
-
+    int i,j,total_cost;
+    printf("Enter no. of vertices:");
+    scanf("%d",&n);
+    
+    printf("\nEnter the cost adjacency matrix:\n");
+    
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&G[i][j]);
+    
+    total_cost=prims();
+    printf("\nspanning tree matrix:\n");
+    
+    for(i=0;i<n;i++)
+    {
+        printf("\n");
+        for(j=0;j<n;j++)
+            printf("%d\t",spanning[i][j]);
+    }
+    
+    printf("\n\nTotal cost of spanning tree=%d",total_cost);
+    return 0;
 }
-/**   
-                                    };   
-        prim(g);    
-      //  return 0;  
-} 
- **/   
+ 
+int prims()
+{
+    int cost[MAX][MAX];
+    int u,v,min_distance,distance[MAX],from[MAX];
+    int visited[MAX],no_of_edges,i,min_cost,j;
+    
+    //create cost[][] matrix,spanning[][]
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+        {
+            if(G[i][j]==0)
+                cost[i][j]=infinity;
+            else
+                cost[i][j]=G[i][j];
+                spanning[i][j]=0;
+        }
+        
+    //initialise visited[],distance[] and from[]
+    distance[0]=0;
+    visited[0]=1;
+    
+    for(i=1;i<n;i++)
+    {
+        distance[i]=cost[0][i];
+        from[i]=0;
+        visited[i]=0;
+    }
+    
+    min_cost=0;        //cost of spanning tree
+    no_of_edges=n-1;        //no. of edges to be added
+    
+    while(no_of_edges>0)
+    {
+        //find the vertex at minimum distance from the tree
+        min_distance=infinity;
+        for(i=1;i<n;i++)
+            if(visited[i]==0 && distance[i] < min_distance)
+            {
+                v=i;
+                min_distance=distance[i];
+            }
+            
+        u=from[v];
+        
+        //insert the edge in spanning tree
+        spanning[u][v]=distance[v];
+        spanning[v][u]=distance[v];
+        no_of_edges--;
+        visited[v]=1;
+        
+        //updated the distance[] array
+        for(i=1;i<n;i++)
+            if(visited[i]==0 && cost[i][v] < distance[i])
+            {
+                distance[i]=cost[i][v];
+                from[i]=v;
+            }
+        
+        min_cost=min_cost+cost[u][v];
+    }
+    
+    return(min_cost);
+}
